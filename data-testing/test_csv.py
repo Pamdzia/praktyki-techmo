bef98fcba35874df07b9f157a9d1205c7f184e57
Pamdzia <ropelklaudia9@gmail.com>
2024-07-15 22:26:13 +0200

    Added new results

--- data-testing/test_csv.py
+++ data-testing/test_csv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # ścieżka do pliku
-csv_path = '../experiments_results/test_results_1207_nEMO.csv'
+csv_path = '../experiments_results/test_results_1507_RAVDESS.csv'
 
 # Wczytanie pliku CSV
 df = pd.read_csv(csv_path)
@@ -11,10 +11,10 @@ emotions = ['angry', 'happy', 'neutral', 'sad']
 filtered_df = df[df['emotion'].isin(emotions)]
 
 # grupowanie po nazwie eksperymentu i emocjach, oraz obliczanie średniej accuracy
-result_df = filtered_df.groupby(['experiment_name', 'emotion']).agg({'accuracy': 'mean'}).reset_index()
+result_df = filtered_df.groupby(['experiment_name', 'emotion']).agg({'recall': 'mean'}).reset_index()
 
 # format tabeli jaki chcemy
-pivot_df = result_df.pivot(index='experiment_name', columns='emotion', values='accuracy').reset_index()
+pivot_df = result_df.pivot(index='experiment_name', columns='emotion', values='recall').reset_index()
 
 # brakujące wartości zerami
 pivot_df = pivot_df.fillna(0)
@@ -22,5 +22,5 @@ pivot_df = pivot_df.fillna(0)
 print(pivot_df)
 
 # wyniki do csv
-output_csv_path = 'nEMO_1207.csv'
+output_csv_path = 'RAVDESS_1507.csv'
 pivot_df.to_csv(output_csv_path, index=False)
