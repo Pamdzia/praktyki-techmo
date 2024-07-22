830d8ebd05de00c324270e9a3b1efe2576332672
Pamdzia <ropelklaudia9@gmail.com>
2024-07-22 21:05:58 +0200

    Added environment setup

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ python3.10 -m venv .venv
 
 source .venv/bin/activate
 pip install --require-virtualenv --upgrade pip
-pip install --require-virtualenv --editable .
+pip install --no-cache --require-virtualenv --editable .
 
 echo "Please remember to activate the environment"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='praktyki-techmo',
-    version='0.9',
+    version='0.9', #Version 1.0 is planned to be the one at the end of the practices
     packages=find_packages(),
     install_requires=required,
 )
