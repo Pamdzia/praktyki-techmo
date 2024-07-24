199b757356f68efcb28113e45c34839022ec692a
Pamdzia <ropelklaudia9@gmail.com>
2024-07-24 21:56:48 +0200

    Added TESS dataset with models and results

---
+++ TESS_models/testing_pipeline.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# ścieżka do skryptu Python
+script_name="/Users/maciejwylecial/Developer/praktyki-Techmo/20_07/praktyki-techmo/training_model/testing.py"
+
+# lista dataset-ów
+datasets=("RAVDESS" "nEMO")
+
+# wyszukiwanie folderów i uruchamianie skryptu dla każdego eksperymentu i datasetu
+for experiment in */ ; do
+    if [ -d "$experiment" ]; then
+        for dataset in "${datasets[@]}"; do
+            experiment_name="${experiment%/}"
+            echo "Uruchamianie eksperymentu dla folderu: $experiment_name i datasetu: $dataset"
+            python "$script_name" "$dataset" "$experiment_name"
+        done
+    fi
+done
+
+echo "Wszystkie eksperymenty zostały przetworzone."
